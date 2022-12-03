383b29c34312d62e950a2ab49527493215211726
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-03 17:34:47 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { rename, readdir } from "node:fs/promises"
+import * as url from "url"
 
-await rename();
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+
+const renameFunc = async () => {
+    try {
+        const copyFolder = await readdir(`${__dirname}/files`)
+        if (
+            copyFolder.filter((file) => file === "wrongFilename.txt").length ===
+                0 ||
+            copyFolder.filter((file) => file === "properFilename.md").length ===
+                1
+        ) {
+            throw new Error("FS operation failed")
+        }
+        await rename(
+            `${__dirname}/files/wrongFilename.txt`,
+            `${__dirname}/files/properFilename.md`
+        )
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+await renameFunc()

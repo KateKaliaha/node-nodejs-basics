edf44de2e764358b9369394a4dbdba376c384b66
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-03 14:46:02 +0300

    feat: add function for create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile, open, readdir } from "node:fs"
+import * as url from "url"
 
-await create();
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+
+const create = async () => {
+    readdir(`${__dirname}/files`, (err, files) => {
+        ;(err) => {
+            if (err) throw err
+        }
+        if (files.includes("fresh.txt")) {
+            console.error("Error: FS operation failed")
+        } else {
+            writeFile(
+                `${__dirname}/files/fresh.txt`,
+                "I am fresh and young",
+                "utf8",
+                (err) => {
+                    if (err) throw err
+                    console.log("File create")
+                }
+            )
+        }
+    })
+}
+
+await create()

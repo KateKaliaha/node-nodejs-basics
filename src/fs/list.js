e5a33eb99cc11f5b60f7f72af99022b00d3793df
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-03 18:19:29 +0300

    feat: add print list of files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,32 @@
-const list = async () => {
-    // Write your code here 
-};
+import { readdir } from "node:fs/promises"
+import * as url from "url"
 
-await list();
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+
+const list = async () => {
+    try {
+        const filesFolder = await readdir(__dirname)
+        if (filesFolder.filter((file) => file === "files").length === 0) {
+            throw new Error("FS operation failed")
+        }
+        await readdir(`${__dirname}files`)
+            .then((files) => {
+                if (files.length === 0) {
+                    throw new Error("FS operation failed")
+                }
+                if (files.length === 0) {
+                    throw new Error("FS operation failed")
+                }
+                console.log(
+                    files.filter((file) => file.split(".").length === 2)
+                )
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+await list()

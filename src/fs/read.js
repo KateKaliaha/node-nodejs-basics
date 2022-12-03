5680b132e4628d86fe30f95c6c2f1830c1777725
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-03 18:51:11 +0300

    feat: add read function for read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
-};
+import { readFile, readdir } from "node:fs/promises"
 
-await read();
+const read = async () => {
+    try {
+        const folderPath = new URL("./files", import.meta.url)
+        const folder = await readdir(folderPath)
+        if (!folder.includes("fileToRead.txt")) {
+            throw new Error("FS operation failed")
+        }
+        const filePath = new URL("./files/fileToRead.txt", import.meta.url)
+        await readFile(filePath, { encoding: "utf8" })
+            .then((contents) => {
+                if (!contents) {
+                    throw new Error("FS operation failed")
+                }
+                console.log(contents)
+            })
+            .catch((err) => {
+                console.error(err.message)
+            })
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+await read()

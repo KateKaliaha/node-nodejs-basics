521c6218471975f15de7c40e41a94ff072f24f4c
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-03 21:26:21 +0300

    feat: complete env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+    const variables = process.env
+    const arr = []
+    for (let key in variables) {
+        if (key.startsWith("RSS_")) {
+            arr.push(`${key}=${variables[key]}`)
+        }
+    }
+    console.log(arr.join("; "))
+}
 
-parseEnv();
+parseEnv()

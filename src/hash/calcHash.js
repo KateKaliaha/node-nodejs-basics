a3a8a8f34a9b16806d114929ef00a38f9dab1401
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-04 12:30:00 +0300

    feat: add function for calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from "node:crypto"
+import { readFile } from "node:fs/promises"
 
-await calculateHash();
+const calculateHash = async () => {
+    try {
+        const folderPath = new URL(
+            "./files/fileToCalculateHashFor.txt",
+            import.meta.url
+        )
+        const contents = await readFile(folderPath, { encoding: "utf8" }).catch(
+            () => {
+                throw new Error("FS operation failed")
+            }
+        )
+        console.log(createHash("sha256").update(contents).digest("hex"))
+    } catch (err) {
+        console.error(err.message)
+    }
+}
+
+await calculateHash()

e87a56eee1ee6798ebb5307c52b19b8c7f485bce
Katherina Kaliaha <mellinda077@mail.ru>
2022-12-05 12:50:41 +0300

    feat: add connect with child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,8 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from "node:child_process"
+const spawnChildProcess = async (...args) => {
+    const filePath = new URL("./files/script.js", import.meta.url)
 
-spawnChildProcess();
+    const child = fork(filePath, ...args)
+}
+
+spawnChildProcess(["arg1", "arg2", "arg3", "arg4"])
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+// const performCalculations = async () => {
+//     // Write your code here
+// };
 
-await performCalculations();
+// await performCalculations();
+
+import * as os from "os"
+import { resolve } from "path"
+import { Worker } from "worker_threads"
+
+const performCalculations = async () => {
+    const numberCores = os.cpus()
+    const arr = []
+    numberCores.forEach((el, ind) => {
+        arr.push(
+            new Promise((res, rej) => {
+                new Worker("./src/wt/worker.js", {
+                    workerData: 10 + ind,
+                })
+                    .on("message", (message) =>
+                        res(console.log({ status: "resolved", data: message }))
+                    )
+                    .on("error", () =>
+                        rej(console.log({ status: "error", data: null }))
+                    )
+            })
+        )
+    })
+
+    await Promise.allSettled(arr)
+}
+
+await performCalculations()
